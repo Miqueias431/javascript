8a97fd5881fca4ffff8bc6a5419b1b88b3aee328
Miqueias Rodrigues <miqueiasr560@gmail.com>
2024-04-29 13:56:33 -0300

    add

---
+++ fundamentos/operadores.js
@@ -0,0 +1,12 @@
+/**
+ * Operadores
+ * Aritméticos, lógicos e comparadores
+ * @author Miqueias Rodrigues
+ */
+
+console.clear()
+console.log("Operadores Aritméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
