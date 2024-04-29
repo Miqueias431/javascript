e2ab293d1eb7c52e9abbd9ca4aecf067952588eb
Miqueias Rodrigues <miqueiasr560@gmail.com>
2024-04-29 14:16:59 -0300

    add

--- fundamentos/operadores.js
+++ fundamentos/operadores.js
@@ -10,3 +10,25 @@ console.log("+ | soma")
 console.log("- | subtração")
 console.log("* | multiplicação")
 console.log("/ | divisão")
+console.log("% | módulo (resto da divisão)")
+// atenção a regra de precedência
+console.log(2+2/2)
+console.log((2+2)/2)
+// resto da divisão
+console.log(10%3)
+
+console.log("Operadores Aritméticos de atribuição")
+console.log("+= | soma e atribui o valor a variável")
+console.log("-= | subtração e atribui o valor a variável")
+console.log("*= | multiplicação e atribui o valor a variável")
+console.log("/= | divisão e atribui o valor a variável")
+console.log("++ | soma 1 do resultado")
+console.log("-- | subtração 1 do resultado")
+
+// exemplos
+let x = 2
+x += 2
+console.log(x)
+x++
+console.log(x)
+
