179e2e7fa5b86b3bbcfd2f04cf1c8e2eb464762a
Miqueias Rodrigues <miqueiasr560@gmail.com>
2024-05-08 15:20:19 -0300

    add

--- fundamentos/funcoes.js
+++ fundamentos/funcoes.js
@@ -44,5 +44,18 @@ let somarA = function (num1, num2) {
     return console.log(num1 + num2)
 }
 
-somarA(2, 3)
+somarA(2, 6)
 console.log(typeof somarA)
+
+// Função anônima simplificada (Arrow function) com retorno
+let somarAF = (num1, num2) => {
+    return console.log(num1 + num2)
+}
+
+somarAF(2, 4)
+console.log(typeof somarAF)
+
+// Função anônima super simplificada (Arrow function) com retorno
+let somarAFS = (num1, num2) => console.log(num1 + num2)
+somarAFS(2, 4)
+console.log(typeof somarAFS)
