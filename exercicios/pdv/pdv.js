e6e6770746dc13c8a9e82fa1705a04330f6e6841
Miqueias Rodrigues <miqueiasr560@gmail.com>
2024-05-15 16:39:20 -0300

    add

---
+++ exercicios/pdv/pdv.js
@@ -0,0 +1,28 @@
+/**
+ * JS - PDV
+ * @author Miqueias Rodrigues
+ */
+
+function calcular() {
+    let total = frmPVD.txtTotal.value
+    let porcentagem = frmPVD.txtPorcentagem.value
+    let desconto = total * (porcentagem / 100)
+    let totalComDesconto = total - desconto
+    frmPVD.txtDesconto.value = desconto
+    frmPVD.txtTotalComDesconto.value = totalComDesconto
+    
+}
+
+function calcularTroco() {
+    let valorPago = frmPVD.txtValorPago.value
+    let totalComDesconto = frmPVD.txtTotalComDesconto.value
+    let troco = valorPago - totalComDesconto
+    frmPVD.txtTroco.value = troco
+   
+}
+
+function limpar() {
+    frmPVD.reset()
+
+}
+
